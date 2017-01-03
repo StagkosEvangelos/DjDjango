ce885dd3094a6f249091997789f7fe91bed21eec
vsnake88 <eyagstag@gmail.com>
2017-01-03 19:41:07 +0200

    3/1/2017

--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     url(r'^$', views.Album_List, name='album_list'),
     #url(r'album/$', views.IndexView.as_view(), name='album'),
-    url(r'^(?P<pk>[0-9]+)/$', views.AlbumDetail, name='detail'),
+    #url(r'^(?P<pk>[0-9]+)/$', views.AlbumDetail, name='detail'),
+    url(r'^(?P<pk>[0-9]+)/$',views.DetailView.as_view(),name='detail'),
+    url(r'^contact/$',views.Contact,name='contact'),
+    url(r'^home/$',views.Home,name='home'),
 ]
